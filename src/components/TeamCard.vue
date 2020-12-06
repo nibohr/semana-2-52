902bd764873d03f51d00f0270b75db308a0a3f9d
Nibohr <nibohr89@gmail.com>
2020-12-06 10:32:34 -0500

    Cambio de imagen por image

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="card bg-white border-0"> 
-            <img :src="member.imagen" class="card-img-top mx-auto d-block mt-3" alt="Foto integrnate del equipo" style="border-radius: 50%; width: 50%;">
+            <img :src="member.image" class="card-img-top mx-auto d-block mt-3" alt="Foto integrnate del equipo" style="border-radius: 50%; width: 50%;">
             <div class="card-body">
                 <h4 class="card-title text-secondary text-center">{{member.nombre}}</h4>
                 <p class="card-text text-secondary text-center"><em>Código: {{member.codigo}}</em></p>
